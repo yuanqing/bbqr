4cd3561cda7bddabb185d127f579b5cee25ccfc4
Yuan Qing <desk@yuanqing.sg>
2018-07-19 17:37:52 +0800

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-const React = require('react')
-const ReactDOM = require('react-dom')
-const App = require('./App')
-
-require('./index.css')
-
-ReactDOM.render(<App />, document.getElementById('root'))
